dcddb1cb1ef0d078e11409bdf0ae81e7610f38e6
Sophie Wollitzer <s.g.wollitzer@wustl.edu>
2025-04-01 21:32:35 -0500

    finished tictactoe game!

--- src/App.jsx
+++ src/App.jsx
@@ -53,16 +53,18 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove,setCurrentMove]=useState(0);
+const xIsNext=currentMove%2===0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+  setHistory(nextHistory);
+  setCurrentMove(nextHistory.length - 1);
   }
   function jumpTo(nextMove){
-
+setCurrentMove(nextMove);
   }
   const moves = history.map((squares, move) => {
     let description;
@@ -72,7 +74,7 @@ export default function Game() {
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
