870d1a4a902db3c3e398f84ab5992e27d366f612
Sophie Wollitzer <s.g.wollitzer@wustl.edu>
2025-04-01 21:04:42 -0500

    setting up a more general game default fn to enable history

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,7 @@ function Square({value,onSquareClick}){
     </button>
   );
 }
-
-export default function Board() {
+ function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -58,6 +57,24 @@ export default function Board() {
     </>
   );
 }
+
+export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
